c80ca4ebce13edfd662f9d4493b3949b10c55b3b
Harshithatmk <89684507+Harshithatmk@users.noreply.github.com>
2021-10-02 12:38:54 +0530

    Create quize_function.h

---
+++ inc/quize_function.h
@@ -0,0 +1,14 @@
+#ifndef __FUNCTIONS_H__
+#define __FUNCTIONS_H__
+
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+
+void show_records();
+void reset_scores();
+void help();
+void edit_scores(float , char []);
+#endif
